e79757b3ade4f5b79ae590fa40f0e3c721570571
yejunjie10311031 <62139629+yejunjie10311031@users.noreply.github.com>
2021-03-06 17:43:53 +0800

    Create xml4.java

---
+++ java/com/mycompany/zhutingxuanbaodalihuan/xml4.java
@@ -0,0 +1,18 @@
+package com.mycompany.zhutingxuanbaodalihuan;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+import java.util.concurrent.*;
+
+public class xml4 extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xml2);
+	}
+}
