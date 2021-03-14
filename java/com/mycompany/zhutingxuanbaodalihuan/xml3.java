3dd8e343ab520c901975c0f420a2ecd2290af5dc
yejunjie10311031 <62139629+yejunjie10311031@users.noreply.github.com>
2021-03-14 09:39:04 +0800

    Add files via upload

--- java/com/mycompany/zhutingxuanbaodalihuan/xml3.java
+++ java/com/mycompany/zhutingxuanbaodalihuan/xml3.java
@@ -8,7 +8,6 @@ import android.content.*;
 import java.util.concurrent.*;
 import android.view.View.*;
 import android.view.ViewGroup.*;
-
 public class xml3 extends Activity
 {
     @Override
@@ -28,6 +27,8 @@ public class xml3 extends Activity
 		Button button4 = (Button) findViewById(R.id.button4);
 		Button button5 = (Button) findViewById(R.id.button5);
 		final weizhi wz1=new weizhi();
+		image1.setX(1);
+		image4.setX(1);
         button1.setOnClickListener(new OnClickListener() {
         
 				@Override
@@ -38,10 +39,8 @@ public class xml3 extends Activity
 					image1.setX(wz1.w1);
 				   image2.setX(wz1.w1);   
 				   image3.setX(wz1.w2);
-				   image4.setX(wz1.w5);
-				   image4.setY(wz1.w6);
-				   text1.setText("坐标"+wz1.w2+"   "+wz1.w3);
-				}
+					text1.setText("坐标"+wz1.w2+"   "+wz1.w3);
+						}
 			});
 		button2.setOnClickListener(new OnClickListener() {
 			
@@ -86,14 +85,12 @@ public class xml3 extends Activity
 			
 		button5.setOnClickListener(new OnClickListener() {
                 int sm1=30;
-				
-				
 				@Override
 				public void onClick(View p1)
 				{
-					int p2=wz1.w5-wz1.w2;
+					float p2=image4.getX()-wz1.w2;
 					int p3=wz1.w3-wz1.w6;
-					if(p2<=120&&p2>=-120&&p3<=130&&p3>=-130)
+					if(p2<=120&&p2>=-120&&p3<=120&&p3>=-120)
 					{
 					sm1=sm1-3;
 					if (sm1==0)
@@ -104,7 +101,43 @@ public class xml3 extends Activity
 				else sm1=sm1+0;
 				}
 			});
-	}
+		new Thread()
+		{
+			public void run()
+			{
+				while (true)
+				{
+					float p1=image4.getX()-image1.getX();
+					if(p1>=80)
+					{
+						try
+						{
+							image4.setX(image4.getX()-18);
+							Thread.sleep(500);
+						}
+						catch (InterruptedException e)
+						{
+
+						}
+					}
+					if(p1<=-80)
+					{
+						try
+						{
+							image4.setX(image4.getX()+18);
+							Thread.sleep(500);
+						}
+						catch (InterruptedException e)
+						{
+
+						}
+					}
+				}
+			}
+		}.start();
+			
+		}
+		
 	public class weizhi
 	{
 		public int w1=190;
@@ -113,7 +146,27 @@ public class xml3 extends Activity
 		public int w4=100;
 		public int w5=1000;
 		public int w6=150;
-	}
+			public void yd1()
+		{
+			int p1=w5-w1;
+			int p2=w6-w3;
+			if(p1>=80)
+			{
+				while(true)
+				{
+					try
+					{
+						TimeUnit.SECONDS.sleep(1);
+					}
+					catch (InterruptedException e)
+					{
+						w5=w5-18;
+					}
+				}
+				}
+			}
+		}
+		
 	public int health1=30;
 	public void fight () 
     {
@@ -130,9 +183,8 @@ public class xml3 extends Activity
 		}
 		catch (InterruptedException e)
 		{
-			finish();
-			Intent intent = new Intent(this,xml3.class);
-			startActivity(intent);
+			
 		}
 	}
-}
+	}
+	
